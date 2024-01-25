291c8ec9699fd912a654881ebd47599aff48dd15
Matheus Augusto Gress Trentin <151881614+MatheusGress@users.noreply.github.com>
2024-01-25 17:43:15 -0300

    Update 1002.cpp

--- CPP/beginner/1002.cpp
+++ CPP/beginner/1002.cpp
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+ 
+int main() {
+ 
+    double A = 0;
+    double n = 3.14159;
+    double R = 0;
+    
+    scanf("%lf", &R);
+ 
+    A = n * (R * R);
+    
+    printf("A=%.4lf\n", A);
+ 
+    return 0;
+}
