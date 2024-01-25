afba2f140eadec12e8be08c9157548496730e309
Matheus Augusto Gress Trentin <151881614+MatheusGress@users.noreply.github.com>
2024-01-25 17:53:14 -0300

    Create 1004.cpp

---
+++ CPP/beginner/1004.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A = 0;
+    int B = 0;
+    int PROD = 0;
+    
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+     PROD= A * B;
+     
+     printf("PROD = %d\n", PROD);
+    
+    return 0;
+}
