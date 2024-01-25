9860ec42066ee6e87391fa37a6399aa761f2bbbe
Matheus Augusto Gress Trentin <151881614+MatheusGress@users.noreply.github.com>
2024-01-25 16:26:17 -0300

    Update 1001.cpp

--- CPP/beginner/1001.cpp
+++ CPP/beginner/1001.cpp
@@ -1,1 +1,18 @@
+#include <stdio.h>
+ 
+int main()
+{
+ 
+    int A = 0;
+    int B = 0;
+    int X = 0;
 
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    X = A + B;
+
+    printf("X = %d\n", X);
+
+    return 0;
+}
