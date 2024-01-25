40d9d2eec219930be10c9c592351d63368674019
Matheus Augusto Gress Trentin <151881614+MatheusGress@users.noreply.github.com>
2024-01-25 17:49:08 -0300

    Update 1003.cpp

--- CPP/beginner/1003.cpp
+++ CPP/beginner/1003.cpp
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A = 0;
+    int B = 0;
+    int SOMA = 0;
+    
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+     SOMA = A + B;
+     
+     printf("SOMA = %d\n", SOMA);
+    
+    return 0;
+}
