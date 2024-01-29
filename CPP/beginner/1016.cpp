85cba09c10035773b8b77f48b00b1c724a0bf5fc
Matheus Augusto Gress Trentin <151881614+MatheusGress@users.noreply.github.com>
2024-01-29 11:47:57 -0300

    Create 1016.cpp

---
+++ CPP/beginner/1016.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int Km = 0, Minutes = 0;
+    
+      scanf("%d", &Km);
+    
+    Minutes = Km * 2;
+    
+      printf("%d minutos\n", Minutes);
+    
+    return 0;
+}
