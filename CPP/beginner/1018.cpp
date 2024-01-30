a149f31103162737d89c63554f67f01db0ed113c
Matheus Augusto Gress Trentin <151881614+MatheusGress@users.noreply.github.com>
2024-01-30 13:43:21 -0300

    Update 1018.cpp

--- CPP/beginner/1018.cpp
+++ CPP/beginner/1018.cpp
@@ -3,19 +3,19 @@
 int main() {
    
     int N = 0, i = 0;
-    int cedulas[] = {100, 50, 20, 10, 5, 2, 1};
-    int quantidades[7] = {0};
+    int BankNotes[] = {100, 50, 20, 10, 5, 2, 1};
+    int quantity[7] = {0};
 
          scanf("%d", &N);
          printf("%d\n", N);
          
     for (i = 0; i < 7; ++i) {
-        quantidades[i] = N / cedulas[i];
-        N %= cedulas[i];
+        quantity[i] = N / BankNotes[i];
+        N %= BankNotes[i];
     }
    
     for (int i = 0; i < 7; ++i) {
-        printf("%d nota(s) de R$ %d,00\n", quantidades[i], cedulas[i]);
+        printf("%d nota(s) de R$ %d,00\n", quantity[i], BankNotes[i]);
     }
 
     return 0;
