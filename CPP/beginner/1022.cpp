4ae24a3cd1352969720d39ba8d676f2d4416a433
Matheus Augusto Gress Trentin <151881614+MatheusGress@users.noreply.github.com>
2024-01-30 15:32:06 -0300

    Update 1022.cpp

--- CPP/beginner/1022.cpp
+++ CPP/beginner/1022.cpp
@@ -1,22 +1,18 @@
 #include <stdio.h>
- 
-int main() {
- 
-   int A = 0, B = 0, C = 0, D = 0, sumAB = 0, sumCD = 0;
-   
-        scanf("%d %d %d %d", &A, &B, &C, &D);
-        
-   sumAB = A + B;     
-   sumCD = C + D;
-   
-   if (B > C && D > A && sumCD > sumAB && C > 0 && D > 0 && A % 2 == 0) {
 
+int main() {
+    int A = 0, B = 0, C = 0, D = 0, sumAB = 0, sumCD = 0;
+    
+       scanf("%d %d %d %d", &A, &B, &C, &D);
+    
+    sumAB = A + B;
+    sumCD = C + D;
+    
+    if (B > C && D > A && sumCD > sumAB && C > 0 && D > 0 && A % 2 == 0) {
         printf("Valores aceitos\n");
-   }    
-     
-    else {
-        
+    } else {
         printf("Valores nao aceitos\n");
-    } 
+    }
+    
     return 0;
 }
