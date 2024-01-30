2d18145481fcb3f95932889af1f7b26e9909ec20
Matheus Augusto Gress Trentin <151881614+MatheusGress@users.noreply.github.com>
2024-01-30 09:12:22 -0300

    Create 1018.cpp

---
+++ CPP/beginner/1018.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+   
+    int N = 0, i = 0;
+    int cedulas[] = {100, 50, 20, 10, 5, 2, 1};
+    int quantidades[7] = {0};
+
+         scanf("%d", &N);
+    
+         printf("%d\n", N);
+         
+    for (i = 0; i < 7; ++i) {
+       
+        quantidades[i] = N / cedulas[i];
+        
+        N %= cedulas[i];
+    }
+        
+    for (int i = 0; i < 7; ++i) {
+        printf("%d nota(s) de R$ %d,00\n", quantidades[i], cedulas[i]);
+    }
+
+    return 0;
+}
