40897c150b7fc3daee390586edc23817811ea549
Matheus Augusto Gress Trentin <151881614+MatheusGress@users.noreply.github.com>
2024-01-30 14:54:18 -0300

    Update 1021.cpp

--- CPP/beginner/1021.cpp
+++ CPP/beginner/1021.cpp
@@ -3,25 +3,24 @@
 int main() {
     
     double N;
-    scanf("%lf", &N);
+        scanf("%lf", &N);
 
     int cents = N * 100;
-
-    int notes[] = {10000, 5000, 2000, 1000, 500, 200}; // Centavos
-    int coins[] = {100, 50, 25, 10, 5, 1}; // Valor em centavos
+    int notes[] = {10000, 5000, 2000, 1000, 500, 200}; 
+    int coins[] = {100, 50, 25, 10, 5, 1}; 
 
     printf("NOTAS:\n");
     for (int i = 0; i < 6; ++i) {
         int count = cents / notes[i];
         cents %= notes[i];
-        printf("%d nota(s) de R$ %.2lf\n", count, notes[i] / 100.0);
+            printf("%d nota(s) de R$ %.2lf\n", count, notes[i] / 100.0);
     }
 
     printf("MOEDAS:\n");
     for (int i = 0; i < 6; ++i) {
         int count = cents / coins[i];
         cents %= coins[i];
-        printf("%d moeda(s) de R$ %.2lf\n", count, coins[i] / 100.0);
+            printf("%d moeda(s) de R$ %.2lf\n", count, coins[i] / 100.0);
     }
 
     return 0;
