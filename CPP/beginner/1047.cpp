643fddca9914d6cddd8f681f05be39ef17e2b2d5
Matheus Augusto Gress Trentin <151881614+MatheusGress@users.noreply.github.com>
2024-02-19 16:29:28 -0300

    Update 1047.cpp

--- CPP/beginner/1047.cpp
+++ CPP/beginner/1047.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
- 
+
 int main() {
- 
-    int HourStart = 0, HourEnd, HourTotal = 0;
+    int HourStart = 0, MinuteStart = 0, HourEnd = 0, MinuteEnd = 0;
+    int TotalHours = 0, TotalMinutes = 0;
     
-        std::cin >> HourStart >> HourEnd;
-    
-    if (HourEnd > HourStart){
-    HourTotal = HourEnd - HourStart;
-        std::cout << "O JOGO DUROU " << HourTotal << " HORA(S)" << std::endl;    
-    } else {
-    HourTotal = (24 - HourStart) + HourEnd;
-        std::cout << "O JOGO DUROU " << HourTotal << " HORA(S)" << std::endl;    
+    std::cin >> HourStart >> MinuteStart >> HourEnd >> MinuteEnd;
+
+    TotalMinutes = (HourEnd * 60 + MinuteEnd) - (HourStart * 60 + MinuteStart);
+
+    if (TotalMinutes < 1) {
+        TotalMinutes += 24 * 60;
     }
-    
+
+    TotalHours = TotalMinutes / 60;
+    TotalMinutes %= 60;
+
+    std::cout << "O JOGO DUROU " << TotalHours << " HORA(S) E " << TotalMinutes << " MINUTO(S)" << std::endl;
+
     return 0;
+}
+
