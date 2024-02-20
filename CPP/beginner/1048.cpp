df10e8947e13a3f9e8680d6daf0b83b5db6370a1
Matheus Augusto Gress Trentin <151881614+MatheusGress@users.noreply.github.com>
2024-02-20 10:29:59 -0300

    Update 1048.cpp

--- CPP/beginner/1048.cpp
+++ CPP/beginner/1048.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <iomanip>
 
-int main () {
+int main() {
+    double salario_atual, novo_salario, aumento;
+    int percentual;
 
-double Salary = 0, Rate = 0, New salary = 0;
-  
-std::cin >> Salary >>
+    std::cin >> salario_atual;
 
+    if (salario_atual <= 400.00) {
+        percentual = 15;
+    } else if (salario_atual <= 800.00) {
+        percentual = 12;
+    } else if (salario_atual <= 1200.00) {
+        percentual = 10;
+    } else if (salario_atual <= 2000.00) {
+        percentual = 7;
+    } else {
+        percentual = 4;
+    }
 
+    aumento = salario_atual * (percentual / 100.0);
+    novo_salario = salario_atual + aumento;
 
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "Novo salario: " << novo_salario << std::endl;
+    std::cout << "Reajuste ganho: " << aumento << std::endl;
+    std::cout << "Em percentual: " << percentual << " %" << std::endl;
 
-
-
-return 0;
+    return 0;
 }
+
