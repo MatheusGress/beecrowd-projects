7875df823b3a45572221c43deb0ce21f290877aa
Matheus Augusto Gress Trentin <151881614+MatheusGress@users.noreply.github.com>
2024-03-05 09:34:13 -0300

    Create 1003.cs

---
+++ CSharp/beginner/1003.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+      int A = 0, B = 0, SOMA = 0;
+       
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+        
+      SOMA = A + B;
+        
+        Console.WriteLine("SOMA = "+ SOMA);
+    }
+
+}
