c554e9fba39daf4e3f710c92731b36983a4484d2
Matheus Augusto Gress Trentin <151881614+MatheusGress@users.noreply.github.com>
2024-09-17 22:20:43 -0300

    Create 1004.cs

---
+++ CSharp/beginner/1004.cs
@@ -0,0 +1,14 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int sum = a * b;
+
+        Console.WriteLine($"PROD = {sum}");
+    }
+}
