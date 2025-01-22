122702c325b03c865f4ef619919cce753273e8db
Matheus Augusto Gress Trentin <151881614+MatheusGress@users.noreply.github.com>
2025-01-22 14:42:01 -0300

    Create 1007.cs

---
+++ CSharp/beginner/1007.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+        int d = int.Parse(Console.ReadLine());
+
+        int diferenca = (a * b) - (c * d);
+
+        Console.WriteLine($"DIFERENCA = {diferenca}");
+    }
+}
